970174b195c16a02587ae25dbc5d2f27a8e70a56
Aditi <aditidarade1@gmail.com>
2024-10-21 13:21:32 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class Question
+{
+    public string questionText;
+    public string optionA;
+    public string optionB;
+    public string optionC;
+    public string optionD;
+    public char correctAnswerLetter;
+    private static char defaultCorrectAnswerLetter = 'X';
+
+    public Question()
+    {
+        //TO DO: Initialize questionText, optionA, optionB, optionC, optionD as null. Initialize correctAnswerLetter to the value of static field 'defaultCorrectAnswerLetter'.
+        questionText = null;
+        optionA = null;
+        optionB = null;
+        optionC = null;
+        optionD = null;
+        correctAnswerLetter = defaultCorrectAnswerLetter;
+    }
+
+    public Question(string questionText)
+    {
+        //TO DO: Initialize questionText. Also, initialize optionA, optionB, optionC, optionD as null. Initialize correctAnswerLetter to the value of static field 'defaultCorrectAnswerLetter'.
+        this.questionText = questionText;
+        optionA = null;
+        optionB = null;
+        optionC = null;
+        optionD = null;
+        correctAnswerLetter = defaultCorrectAnswerLetter;
+    }
+
+    public Question(string questionText, string optionA, string optionB, string optionC, string optionD, char correctAnswerLetter)
+    {
+        //TO DO: Initialize questionText, optionA, optionB, optionC, optionD and correctAnswerText. Validate the value of correctAnswerLetter. It should either 'A', 'B', 'C' or 'D' only
+        this.questionText = questionText;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        if (correctAnswerLetter == 'A' || correctAnswerLetter == 'B' || correctAnswerLetter == 'C' || correctAnswerLetter == 'D')
+        {
+            this.correctAnswerLetter = correctAnswerLetter;
+        }
+        else
+        {
+            this.correctAnswerLetter = defaultCorrectAnswerLetter;
+        }
+    }
+
+   
+
+    public bool AreOptionsValid()
+    {
+        //TO DO: Return true, if at least two options are not null
+        if ((optionA != null && optionB != null) || (optionA != null && optionC != null) || (optionA != null && optionD != null) || (optionB != null && optionC != null) || (optionB != null && optionD != null) || (optionC != null && optionD != null))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Sec8_Assign21_QuestionClass/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //TO DO: Create an object of Question class and pass no arguments to the constructor
+        Question q1 = new Question();
+        //TO DO: Create an object of Question class and pass value for questionText only to the constructor.
+        Question q2 = new Question("What is the capital of the USA?");
+        //TO DO: Create an object of Question class and pass values for questionText, optionA, optionB, optionC, optionD and correctAnswerLetter to the constructor.
+        Question q3 = new Question("What is the capital of the USA?", "London", "New York", "Washington, D.C.", "San Diego", 'C');
+        //TO DO: Create an object of Question class and pass values for questionText, optionA, optionB, optionC, optionD only to the constructor.
+        Question q4 = new Question() {questionText = "What is the capital of the USA?", optionA = "London", optionB = "New York", optionC = "Washington, D.C.", optionD = "San Diego" };
+    }
+}
